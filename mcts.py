8e211efb154597c99927685bc9cec56614498936
ffbskt <ffbskt@gmail.com>
2019-09-24 15:33:13 +0300

    One epoch study work (prob_pred == Q_real, before try whith N)

--- mcts.py
+++ mcts.py
@@ -9,27 +9,32 @@ class dotdict(dict):
         return self[name]
 
 class Node:
-    def __init__(self, formula, predP, predR, fin_reward=0):
+    def __init__(self, formula, predP, predR, fin_reward=0, Q=0):
         self.formula = formula
         self.predP = predP
         self.predR = predR
         self.fin_reward = fin_reward
         self.N = 0
-        self.Q = 0
+        self.Q = Q
 
     def __lt__(self, other):
         if self.fin_reward:
             return False
         if other.fin_reward:
             return True
-        return self.predR > other.predR
+        #return self.predR > other.predR # ??? predict R or computed Q
+        return self.Q > other.Q
 
 class MCTS_best_leaf:
     """
         different algorithm. The Idea do not go down by tree choose best ucb node, except - we immediatly
         compare:
      1) leafs and go to best leaf even parents branch has low ucb.
-     2) compute_Q - no value 
+     2) compute_Q - no value
+
+     Concrete problem of this algorithm: real probability we extimate by time visited, but most formula is leaf or
+     we never choose it to expand, so p_next = [0 0 0 0 0] and only some N_visited grow exponentialy to root.
+     The reward learn better just because smoose by squard and special formula.
 
     """
     def __init__(self, env, model, args):
@@ -37,12 +42,13 @@ class MCTS_best_leaf:
         self.args = args
         node = Node('', 1, 0.5)
         self.heap_best = [node,]
-        self.Nodes = {'': node}
+        self.Nodes = {'':node}
         #self.Ns[node.formula]
         self.model = model
         #self.samples = []
 
     def find_best_leaf(self):
+        #print(n. self.heap_best)
         return heapq.heappop(self.heap_best)
 
     def expand(self, node):
@@ -52,7 +58,7 @@ class MCTS_best_leaf:
         for a in range(self.env.n_actions):
             r, next_formula = self.env.do_move(node.formula, a)
             Q = self.compute_Q(pred_P[a], pred_R[a], next_formula)
-            new_node = Node(next_formula, pred_P[a], pred_R[a], fin_reward=r)
+            new_node = Node(next_formula, pred_P[a], pred_R[a], fin_reward=r, Q=Q)
             #print(next_formula, r)
             heapq.heappush(self.heap_best, new_node) ###&&&
             if r != 0:
@@ -72,6 +78,7 @@ class MCTS_best_leaf:
             self.Nodes[formula[:-i]].N += 1
 
     def compute_Q(self, pred_P, pred_R, formula):
+
         # print(formula, pred_R,  self.args.cpuct * pred_P * np.sqrt(self.Ns[formula[:-1]]), self.Ns[formula[:-1]], self.Ns, formula[:-1])
         return pred_R + self.args.cpuct * pred_P * np.sqrt(self.Nodes[formula[:-1]].N)
 
@@ -82,7 +89,6 @@ class MCTS_best_leaf:
             r = max(m.expand(node), r)
             i += 1
 
-        # TODO add_real_reward(self.Nodes)
         self.add_real_reward()
 
         return self.Nodes.values()
@@ -96,6 +102,26 @@ class MCTS_best_leaf:
                 positive_ind = i
             self.Nodes[f].fin_reward *= np.sqrt(positive_ind/(1 + i))
 
+    def get_next_node_property(self, formula):
+        """
+        from formula give all children probabilitys and rewards
+        ! do only after self.add_real_reward!
+        :return: action size prob and reward
+        """
+        probs = np.zeros([self.env.n_actions])
+        rewards = np.zeros([self.env.n_actions])
+        for i, a in enumerate(self.env.action_space):
+            child = formula + a
+            if child in self.Nodes:
+                probs[i] = self.Nodes[child].Q
+                rewards[i] = self.Nodes[child].fin_reward
+        if np.sum(probs):
+            probs = probs / np.sum(probs)
+        else:
+            probs += 1
+            probs = probs / np.sum(probs)
+        return probs, rewards
+
 
 
 
@@ -103,7 +129,7 @@ if __name__ == "__main__":
     model = Model()
     env = Env(inp=1, out=1)
 
-    args = dotdict({'cpuct':2, 'iters':22})
+    args = dotdict({'cpuct':2, 'iters':200})
     m = MCTS_best_leaf(env, model, args)
 
 
@@ -141,15 +167,25 @@ if __name__ == "__main__":
 
 
 
+
     def train_model(nodes_buc):
-        for i in range(20):
-            batch = get_batch(nodes_buc, batch_size=10)
+        for i in range(500):
+            batch = get_batch(nodes_buc, batch_size=35)
             # print(batch)
             X = np.vstack([prepare_input(env, env.inp, env.out, node.formula)
                            for node in batch])
-            reward = np.vstack([np.array([node.fin_reward]) for node in batch])
-            probability = np.vstack([np.array([node.predP]) for node in batch])
-            model.training(X, reward, probability)
+            reward, probability = None, None
+            for node in batch:
+                prob, rew = m.get_next_node_property(node.formula)
+                if reward is not None:
+                    reward = np.vstack([reward, rew])
+                    probability = np.vstack([probability, prob])
+                else:
+                    reward = rew
+                    probability = prob
+
+            # print(prob)
+            model.training_(X, reward, probability)
 
 
     val = list(m.sampling())
@@ -160,52 +196,12 @@ if __name__ == "__main__":
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#    import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt
     #print(policy_net.loss_backet)    
-#    plt.plot(policy_net.loss_backet)
-#    plt.show()
+    plt.plot(model.loss_backet)
+    plt.show()
+    plt.plot(sorted([m.Nodes[f].Q for f in m.Nodes]))
+    plt.show()
     
 
 
